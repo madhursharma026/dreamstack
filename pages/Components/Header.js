8d2e329f7d341ddb62d76539004e81771f2cc0c7
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-01-22 22:45:40 +0530

    Initial Commit

---
+++ pages/Components/Header.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import styles from "../../styles/Header.module.css";
+
+function Header() {
+    return (
+        <Navbar className={`fixed-top  px-xl-0 px-xl-5`} expand="md">
+            <Container>
+                <Navbar.Brand>
+                    <Link href='/'>
+                        <img src="https://dreamstack.netlify.app/media/img/logos/logo.jpg" alt="#ImgNotFound" width="30px" height='30px' />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" className={`${styles.navbarToggle}`} />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className={`me-auto my-2 my-lg-0 ${styles.navOptionBeforeLG}`} navbarScroll>
+                        <span className={`px-3 ${styles.menuOptions}`}><b>What We Do</b></span>
+                        <span className={`px-3 ${styles.menuOptions}`}><b>Who We Are</b></span>
+                        <span className={`px-3 ${styles.menuOptions}`}><b>Careers</b></span>
+                        <span className={`px-3 ${styles.menuOptions}`}><b>Terms</b></span>
+                    </Nav>
+                    <div className={`${styles.navOptionBeforeLG}`}>
+                        <button type="button" className={`btn btn-outline-primary d-flex px-3 ${styles.downloadBtn}`}><b>Download Dreamstack</b></button>
+                    </div>
+                    <Nav className={`me-auto my-2 my-lg-0 ${styles.navOptionAfterLG}`} navbarScroll>
+                        <div style={{ display: "flex", justifyContent: "center" }}>
+                            <button type="button" className={`btn btn-outline-primary d-flex px-3 ${styles.downloadBtn}`}><b>Download Dreamstack</b></button>
+                        </div>
+                        <hr />
+                        <p className={`px-3 pt-1 ${styles.menuOptionsAfterLG}`}><b>What We Do</b></p>
+                        <hr className={`${styles.hrStyle}`} />
+                        <p className={`px-3 pt-1 ${styles.menuOptionsAfterLG}`}><b>Who We Are</b></p>
+                        <hr className={`${styles.hrStyle}`} />
+                        <p className={`px-3 pt-1 ${styles.menuOptionsAfterLG}`}><b>Careers</b></p>
+                        <hr className={`${styles.hrStyle}`} />
+                        <p className={`px-3 pt-1 ${styles.menuOptionsAfterLG}`}><b>Terms</b></p>
+                        <hr className={`${styles.hrStyle}`} />
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
