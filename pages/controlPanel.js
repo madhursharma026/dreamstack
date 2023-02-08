46e0357a03ed18124828c729614dbe19b435e8cd
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-02-09 00:12:23 +0530

    Control Panel Page Created!

---
+++ pages/controlPanel.js
@@ -0,0 +1,11 @@
+import Content from "./Components/controlPanel/Content";
+import Header from "./Components/controlPanel/Header";
+
+export default function controlPanel() {
+  return (
+    <>
+    <Header />
+    <Content />
+    </>
+  )
+}
