de7154f5d2ef3c6561d4eb0cabbdbeaa11abe5b4
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-02-15 15:07:10 +0530

    Wrong File added

--- _app.js
+++
@@ -1,9 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <div style={{ fontFamily: 'Inter,X-LocaleSpecific,sans-serif' }}>
-      <Component {...pageProps} />
-    </div>
-  )
-}
