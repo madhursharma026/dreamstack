bedde3a6ec8cd1f6a239235c89d68421edd4c865
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-04-30 21:09:32 +0530

    Changes Done!

--- pages/_document.js
+++ pages/_document.js
@@ -2,10 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" style={{overflowX:"hidden"}}>
+    <Html lang="en" style={{ overflowX: "hidden" }}>
       <Head />
       <body>
         <Main />
+        <head>
+          <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+        </head>
         <NextScript />
       </body>
     </Html>
